7a912b4c36634cd49dec48b7da3301e7d208eaad
Bakhrom0904 <bahromislomov0904@gmail.com>
2023-02-07 23:46:24 +0500

    working with routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::prefix('admin')->group(function () {
+    
+    Route::get('/users', function () {
+        return view('users');
+    })->name('users');
+
+});
